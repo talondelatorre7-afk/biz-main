6eebce7f835d12c24a49295c39fb1b416b8ee0b8
Talon De La Torre <tddmedia1@gmail.com>
2026-04-17 11:11:49 -0700

    Remove address details from client information

--- src/_data/client.js
+++ src/_data/client.js
@@ -3,15 +3,6 @@ module.exports = {
     email: "help@deepsea.digital",
     phoneForTel: "909-919-0912",
     phoneFormatted: "(909) 919-0912",
-    address: {
-        lineOne: "14406 Frostburg Ave",
-        lineTwo: "Second Address Line",
-        city: "Chino",
-        state: "CA",
-        zip: "91710",
-        country: "US",
-        mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
-    },
     socials: {
         facebook: "https://www.facebook.com/",
         instagram: "https://www.instagram.com/",
